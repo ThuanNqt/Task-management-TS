6df034393752e87dfea380a3e3f79828ceb2801f
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-03-22 21:34:30 +0700

    4. Configure the project according to the MVC model

---
+++ api/v1/controllers/task.controller.ts
@@ -0,0 +1,13 @@
+import Task from "../models/task.model";
+import { Request, Response } from "express";
+
+export const index = async (req: Request, res: Response): Promise<void> => {
+  const tasks = await Task.find({ deleted: false });
+  res.json(tasks);
+};
+
+export const detail = async (req: Request, res: Response): Promise<void> => {
+  const id: string = req.params.id;
+  const task = await Task.findOne({ _id: id, deleted: false });
+  res.json(task);
+};
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
-import Task from "./models/task.model";
+import mainV1Routes from "./api/v1/routes/index.route";
 dotenv.config();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
@@ -9,16 +9,8 @@ const port: number | string = process.env.PORT || 3000;
 // Connect to database
 database.connect();
 
-app.get("/tasks", async (req: Request, res: Response) => {
-  const tasks = await Task.find({ deleted: false });
-  res.json(tasks);
-});
-
-app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
-  const id: string = req.params.id;
-  const task = await Task.findOne({ _id: id, deleted: false });
-  res.json(task);
-});
+// Router
+mainV1Routes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
