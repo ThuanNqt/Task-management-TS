df2dbf1b50c2b60799f680396340bc17fb8a4147
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-03-23 01:30:08 +0700

    12. Register and Login user

---
+++ api/v1/controllers/user.controller.ts
@@ -0,0 +1,77 @@
+import md5 from "md5";
+import User from "../models/user.model";
+import { generateRandomString } from "../../../helpers/generate";
+import { Request, Response } from "express";
+
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const existEmail = await User.findOne({
+      email: req.body.email,
+      deleted: false,
+    });
+    console.log(existEmail);
+    if (existEmail) {
+      res.json({
+        code: 400,
+        message: "Email already exist!",
+      });
+    } else {
+      req.body.password = md5(req.body.password);
+      req.body.token = generateRandomString(30);
+
+      const user = new User(req.body);
+      await user.save();
+
+      const token = user.token;
+
+      res.json({
+        code: 200,
+        message: "Register successful!",
+        token: token,
+      });
+    }
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Register fail!",
+    });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+      deleted: false,
+    });
+
+    if (!user) {
+      res.json({
+        code: 400,
+        message: "Email does not exist!",
+      });
+      return;
+    }
+
+    if (user.password !== md5(req.body.password)) {
+      res.json({
+        code: 400,
+        message: "Password incorrect!",
+      });
+      return;
+    }
+
+    const token = user.token;
+
+    res.json({
+      code: 200,
+      message: "Login successful!",
+      token: token,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Login fail!",
+    });
+  }
+};
---
+++ helpers/generate.ts
@@ -0,0 +1,20 @@
+export const generateRandomString = (length: number): string => {
+  let result: string = "";
+  const characters: string =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength: number = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+export const generateRandomNumber = (length: number): string => {
+  let result: string = "";
+  const characters: string = "0123456789";
+  const charactersLength: number = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
