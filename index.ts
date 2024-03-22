045ce3cd7086dc8c1f7a20d5309034fb60702d63
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-03-23 00:20:46 +0700

    11. Cors

--- index.ts
+++ index.ts
@@ -3,9 +3,13 @@ import * as database from "./config/database";
 import dotenv from "dotenv";
 import mainV1Routes from "./api/v1/routes/index.route";
 import bodyParser = require("body-parser");
+import cors from "cors";
 dotenv.config();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+// cors
+app.use(cors);
 
 // parse application/json
 app.use(bodyParser.json());
