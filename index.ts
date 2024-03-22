fa31a2a1806622861aad4c5b7f8901df397a4d40
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-03-22 19:36:06 +0700

    2. Get a list tasks

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("OK");
+import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+import Task from "./models/task.model";
+dotenv.config();
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+// Connect to database
+database.connect();
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({ deleted: false });
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
