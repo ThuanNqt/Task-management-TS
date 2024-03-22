1ce0617a3ac924ce328088f6930be895eb859cc9
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-03-23 01:57:13 +0700

    14. Tranfer router to private router

--- api/v1/controllers/user.controller.ts
+++ api/v1/controllers/user.controller.ts
@@ -78,16 +78,10 @@ export const login = async (req: Request, res: Response) => {
 
 export const detail = async (req: Request, res: Response) => {
   try {
-    const id: string = req.params.id;
-    const user = await User.findOne({
-      _id: id,
-      deleted: false,
-    }).select("-password -token");
-
     res.json({
       code: 200,
       message: "Get info successful!",
-      data: user,
+      data: req["user"],
     });
   } catch (error) {
     res.json({
--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -1,10 +1,11 @@
+import * as authMiddleware from "./../middlewares/auth.middleware";
 import { taskRoutes } from "./task.route";
 import { userRoutes } from "./user.route";
 import { Express } from "express";
 
 const mainV1Routes = (app: Express): void => {
   const version = "/api/v1";
-  app.use(version + "/tasks", taskRoutes);
+  app.use(version + "/tasks", authMiddleware.requireAuth, taskRoutes);
   app.use(version + "/users", userRoutes);
 };
 
