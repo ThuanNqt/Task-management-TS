f7f6f6c6c0ddd4cb00313abdb323234d0061430f
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-03-22 22:53:25 +0700

    6. Filter by keyword

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,10 +1,14 @@
 import Task from "../models/task.model";
 import { Request, Response } from "express";
+import paginationHelper from "../../../helpers/pagination";
+import searchHelper from "../../../helpers/search";
 
 export const index = async (req: Request, res: Response): Promise<void> => {
+  //find
   interface Find {
     deleted: boolean;
     status?: string;
+    title?: RegExp;
   }
 
   const find: Find = {
@@ -15,7 +19,36 @@ export const index = async (req: Request, res: Response): Promise<void> => {
     find.status = req.query.status.toString();
   }
 
-  const tasks = await Task.find(find);
+  // sort
+  const sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey.toString()] = req.query.sortValue;
+  }
+
+  // pagination
+  let initPagination = {
+    currentPage: 1,
+    limitItems: 2,
+  };
+
+  const countTasks = await Task.countDocuments(find);
+  const objectPagination = paginationHelper(
+    initPagination,
+    req.query,
+    countTasks
+  );
+
+  // filter by keyword
+  let objectSearch = searchHelper(req.query);
+  if (req.query.keyword) {
+    find.title = objectSearch.regex;
+  }
+
+  const tasks = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
+
   res.json(tasks);
 };
 
---
+++ helpers/pagination.ts
@@ -0,0 +1,29 @@
+interface ObjectPagination {
+  currentPage: number;
+  limitItems: number;
+  skip?: number;
+  totalPage?: number;
+}
+
+const paginationHelper = (
+  objectPagination: ObjectPagination,
+  query: Record<string, any>,
+  countTasks: number
+): ObjectPagination => {
+  if (query.page) {
+    objectPagination.currentPage = parseInt(query.page);
+  }
+
+  if (query.limit) {
+    objectPagination.limitItems = parseInt(query.limit);
+  }
+
+  objectPagination.skip =
+    (objectPagination.currentPage - 1) * objectPagination.limitItems;
+  objectPagination.totalPage = Math.ceil(
+    countTasks / objectPagination.limitItems
+  );
+  return objectPagination;
+};
+
+export default paginationHelper;
